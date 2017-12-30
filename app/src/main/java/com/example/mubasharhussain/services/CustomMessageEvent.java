2b058ab16ec782ab1f2d51c0ca1a71b2c1bd8464
MubasharHussain0324 <mubasharhussain13@yahoo.com>
2017-12-30 09:48:55 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mubasharhussain/services/CustomMessageEvent.java
@@ -0,0 +1,19 @@
+package com.example.mubasharhussain.services;
+
+/**
+ * Created by MubasharHussain on 12/30/2017.
+ */
+
+public class CustomMessageEvent {
+
+
+        private int limit;
+
+        public int getLimit() {
+            return limit;
+        }
+
+        public void setLimit(int limit) {
+            this.limit = limit;
+        }
+}
